aebe624ab6459afe37d1c5470b82b3facc5a1079
Glitch (warp-aphid) <none>
2018-07-06 18:43:22 +0000

    working version, ready for testing on FCC

--- server.js
+++ server.js
@@ -5,6 +5,8 @@
 var express = require('express');
 var app = express();
 
+app.set('trust proxy');
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -20,8 +22,15 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+
+app.get('/api/whoami', (req, res)=>{
+  let address = req.headers['x-forwarded-for'].split(',');
+    
+  res.json({
+    "ipaddress": address[0],
+    "language": req.headers['accept-language'],
+    "software": req.headers['user-agent']
+  })
 });
 
 
